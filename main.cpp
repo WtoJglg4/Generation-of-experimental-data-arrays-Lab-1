50506371aae2658aa89e95f81ada4007ff79fd01
WtoJglg4 <glazovv81@gmail.com>
2023-10-15 12:48:07 +0300

    Functions have been restructured, function pointers and error handlers have been added.

--- main.cpp
+++ main.cpp
@@ -1,107 +1,335 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
 using namespace std;
 
-const int MIN = 10, MAX = 20, length = 50, intervalLen = 30;
+//CONSTANTS
+const int   SEQUENCES_QUANTITY = 6,
+            ORDERED = 3,
+            PART_ORDERED = 3,
+            MIN_INT = -1000,
+            MAX_INT = 1000,
+            LENGTH = 1000,
+            INTERVAL_LENGTH = 570000;
+const double    MIN_DOUBLE = -1000,
+                MAX_DOUBLE = 1000;
 
-//print array
-void PrintArr(int Array[], int len){
-    cout << "Array: \n[";
-    for (int i = 0; i < len; i++){
-        if (i != len - 1){
-            cout << Array[i] << ", ";
-        } else{
-            cout << Array[i] << "]\n";
+//GENERATING INTEGER SEQUENCES
+void AscendingSequenceInt(int[], int);
+void DescendingSequenceInt(int[], int);
+void RandomSequenceInt(int[], int);
+void SawSequenceInt(int[], int, int, int, int);
+void SinSequenceInt(int[], int, int, int, int);
+void StepSequenceInt(int[], int, int, int, int);
+
+//GENERATING DOUBLE SEQUENCES
+void AscendingSequenceDouble(double[], int);
+void DescendingSequenceDouble(double[], int);
+void RandomSequenceDouble(double[], int);
+void SawSequenceDouble(double[], int, int, double, double);
+void SinSequenceDouble(double[], int, int, double, double);
+void StepSequenceDouble(double[], int, int, double, double);
+
+//PRINTING SEQUENCES
+void PrintArrConsoleInt(int[], int); 
+void PrintArrFileInt(int[], int, FILE*);
+void PrintArrConsoleDouble(double[], int);
+void PrintArrFileDouble(double[], int, FILE*);
+
+//POINTERS INTEGER FUNCTIONS
+void (*funcInt1)(int[], int) = AscendingSequenceInt;
+void (*funcInt2)(int[], int) = DescendingSequenceInt;
+void (*funcInt3)(int[], int) = RandomSequenceInt;
+void (*funcInt4)(int[], int, int, int, int) = SawSequenceInt;
+void (*funcInt5)(int[], int, int, int, int) = SinSequenceInt;
+void (*funcInt6)(int[], int, int, int, int) = StepSequenceInt;
+void(*IntOrdered[ORDERED])(int[], int) ={funcInt1, funcInt2, funcInt3};
+void(*IntPartOrdered[PART_ORDERED])(int[], int, int, int, int) ={funcInt4, funcInt5, funcInt6};
+
+
+//POINTERS DOUBLE FUNCTIONS
+void (*funcDouble1)(double[], int) = AscendingSequenceDouble;
+void (*funcDouble2)(double[], int) = DescendingSequenceDouble;
+void (*funcDouble3)(double[], int) = RandomSequenceDouble;
+void (*funcDouble4)(double[], int, int, double, double) = SawSequenceDouble;
+void (*funcDouble5)(double[], int, int, double, double) = SinSequenceDouble;
+void (*funcDouble6)(double[], int, int, double, double) = StepSequenceDouble;
+void(*DoubleOrdered[ORDERED])(double[], int) ={funcDouble1, funcDouble2, funcDouble3};
+void(*DoublePartOrdered[PART_ORDERED])(double[], int, int, double, double) ={funcDouble4, funcDouble5, funcDouble6};
+
+//ERROR HANDLERS
+int ErrorHandler(int, int, double, double, int, int);
+void PrintError(int);
+
+//FILES
+//INT
+const char AscendingInt[] = "output/AscendingInt.txt";
+const char DescendingInt[] = "output/DescendingInt.txt";
+const char RandomInt[] = "output/RandomInt.txt";
+const char SawInt[] = "output/SawInt.txt";
+const char SinInt[] = "output/SinInt.txt";
+const char StepInt[] = "output/StepInt.txt";
+//DOUBLE
+const char AscendingDouble[] = "output/AscendingDouble.txt";
+const char DescendingDouble[] = "output/DescendingDouble.txt";
+const char RandomDouble[] = "output/RandomDouble.txt";
+const char SawDouble[] = "output/SawDouble.txt";
+const char SinDouble[] = "output/SinDouble.txt";
+const char StepDouble[] = "output/StepDouble.txt";
+const char* FilesInt[6] = {AscendingInt, DescendingInt, RandomInt, SawInt, SinInt, StepInt};
+const char* FilesDouble[6] = {AscendingDouble, DescendingDouble, RandomDouble, SawDouble, SinDouble, StepDouble};
+
+
+
+
+int main(){
+    int error = ErrorHandler(MIN_INT, MAX_INT, MIN_DOUBLE, MAX_DOUBLE, LENGTH, INTERVAL_LENGTH);
+    if (error != 0){
+        PrintError(error);
+        return error;
+    }
+
+    int ArrayInt[LENGTH];
+    double ArrayDouble[LENGTH];
+
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < ORDERED; j++){
+            if (i == 0){
+                FILE *file = fopen(FilesInt[j], "w");
+                IntOrdered[j](ArrayInt, LENGTH);
+                PrintArrFileInt(ArrayInt, LENGTH, file);
+                fclose(file);
+            } else{
+                FILE *file = fopen(FilesDouble[j], "w");
+                DoubleOrdered[j](ArrayDouble, LENGTH);
+                PrintArrFileDouble(ArrayDouble, LENGTH, file);
+                fclose(file);
+            }
         }
     }
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < PART_ORDERED; j++){
+            if (i == 0){
+                FILE *file = fopen(FilesInt[j + PART_ORDERED], "w");
+                IntPartOrdered[j](ArrayInt, LENGTH, INTERVAL_LENGTH, MIN_INT, MAX_INT);
+                PrintArrFileInt(ArrayInt, LENGTH, file);
+                fclose(file);
+            } else{
+                FILE *file = fopen(FilesDouble[j + PART_ORDERED], "w");
+                DoublePartOrdered[j](ArrayDouble, LENGTH, INTERVAL_LENGTH, MIN_DOUBLE, MAX_DOUBLE);
+                PrintArrFileDouble(ArrayDouble, LENGTH, file);
+                fclose(file);
+            }
+        }
+    }
+    
+} 
+
+//error handler
+int ErrorHandler(int minInt, int maxInt, double minDouble, double maxDouble, int length, int intervalLength){
+    if (maxInt < minInt) return 1;
+    if (maxDouble < minDouble) return 2;
+    if (length < 1) return 3;
+    if (length < intervalLength) return 4;
+    if (intervalLength < 1) return 5;
+    return 0;
 }
 
-//generates an ascending sequence
-void AscendingSequence(int Array[], int len){
-    float k = ((MAX - MIN) * 1.0) / (len * 1.0), b = MIN;
-    cout << k << endl;
+//print an error
+void PrintError(int error){
+    printf("ERROR: ");
+    switch (error){
+        case 1:
+            printf("MIN_INT must be less then MAX_INT. error code: %d\n", error);
+            break;
+        case 2:
+            printf("MIN_DOUBLE must be less then MAX_DOUBLE. error code: %d\n", error);
+            break;
+        case 3:
+            printf("LENGTH must be greater then 0. error code: %d\n", error);
+            break;
+        case 4:
+            printf("INTERVAL_LENGTH must be less then LENGTH. error code: %d\n", error);
+            break;
+        case 5:
+            printf("INTERVAL_LENGTH must be greater then 0. error code: %d\n", error);
+            break;
+    }
+}
+
+
+//print int array to console
+void PrintArrConsoleInt(int Array[], int len){
+    for (int i = 0; i < len; i++){
+        char end[3];
+        if (i != len - 1){
+            strcpy(end, ", ");
+        } else{
+            strcpy(end, "]\n");
+        }
+        printf("%d%s", Array[i], end);
+    }
+}
+
+//print int array to file
+void PrintArrFileInt(int Array[], int len, FILE *file){
+    for (int i = 0; i < len; i++){
+        fprintf(file, "%d\n", Array[i]);
+    }
+    fprintf(file, "\n");
+}
+
+//generates an ascending integer sequence
+void AscendingSequenceInt(int Array[], int len){
+    double k = ((MAX_INT - MIN_INT) * 1.0) / (len * 1.0), b = MIN_INT;
     for (int i = 0; i < len; i++){
         Array[i] = k * i + b;
     }
 }
 
-//generates a descending sequence
-void DescendingSequence(int Array[], int len){
-    float k = ((MAX - MIN) * -1.0) / (len * 1.0), b = MAX;
+//generates a descending integer sequence
+void DescendingSequenceInt(int Array[], int len){
+    double k = ((MAX_INT - MIN_INT) * -1.0) / (len * 1.0), b = MAX_INT;
     for (int i = 0; i < len; i++){
         Array[i] = k * i + b;
     }
 }
 
-//generates a descending sequence
-void RandomSequence(int Array[], int len){
+//generates a random integer sequence
+void RandomSequenceInt(int Array[], int len){
     srand(time(NULL));
     for (int i = 0; i < len; i++){
-        Array[i] = MIN + rand() % (MAX - MIN + 1);
+        Array[i] = MIN_INT + rand() % (MAX_INT - MIN_INT + 1);
     }
 }
 
-//generates a sawtooth sequence
-void SawSequence(int Array[], int len, int intervalLen, int min, int max){
-    if (intervalLen == 0){
-        cout << "ERROR: devision by zero";
-        return;
-    }
-    float k = ((max - min) * 1.0) / (intervalLen * 1.0);
-    int b = min;
+//generates a saw integer sequence
+void SawSequenceInt(int Array[], int len, int intervalLen, int min, int max){
+    double k = ((max - min) * 1.0) / (intervalLen * 1.0);
     for (int i = 0; i < len; i++){
-        // Array[i] = (int)(k * i) % b + b;
-        Array[i] = ((int)(k * i)) % (max - min) + b;
+        Array[i] = (int)(k * (i % intervalLen)) + min;
     }
 }
 
-//generates a sinusoidal sequence
-void SinSequence(int Array[], int len, int intervalLen, int min, int max){
-    float x = 2 * M_2_PI / intervalLen;
+//generates a sinusoidal integer sequence
+void SinSequenceInt(int Array[], int len, int intervalLen, int min, int max){
+    double x = 2 * M_PI / (intervalLen * 1.0);
     int k = (max - min) / 2;
     for (int i = 0; i < len; i++){
         Array[i] = k * sin(i * x) + min + k;
-        //cout << Array[i] << " " << i * x << " " << sin(i * x) <<  endl;
     }
 }
 
-//generates a step sequence
-void StepSequence(int Array[], int len, int intervalLen, int min, int max){
+
+//generates a step integer sequence
+void StepSequenceInt(int Array[], int len, int intervalLen, int min, int max){
     int intMin, intMax, intCount, stepHigh;
     if (len % intervalLen == 0){
-        intCount = len / intervalLen - 1;
+        intCount = len / intervalLen;
     } else{
         intCount = len / intervalLen + 1;
     }
-    stepHigh = (MAX - MIN) / intCount;
-    intMin = MIN;
-    intMax = MIN + stepHigh;
+    
+    stepHigh = (max - min) / intCount;
+    intMin = min;
+    intMax = min + stepHigh;
 
     srand(time(NULL));
     for (int i = 0; i < len; i++){
-        if (i > 0 && i < len - 1 && (i + 1) % intervalLen == 0 ){
+        if (i > 0 && i < len - 1 && i % intervalLen == 0 &&  (i + 1) / intervalLen > 0){
             intMin = intMax;
             intMax += stepHigh;
         }
-        Array[i] = intMin + rand() % (intMax - intMin + 1);
-        //cout << Array[i] << " " << i << " " << intMin << " " << intMax << endl;
+        Array[i] = rand() % (intMax - intMin + 1) + intMin;
     }
 }
 
-// const int MIN = 10, MAX = 20, length = 50, intervalLen = 10;
 
-int main(){
-    int A[length];
-    // AscendingSequence(A, length);
-    // PrintArr(A, length); 
-    // DescendingSequence(A, length);
-    // PrintArr(A, length); 
-    // RandomSequence(A, length);
-    // PrintArr(A, length); 
-    // SawSequence(A, length, intervalLen, MIN, MAX);
-    // PrintArr(A, length); 
-    // SinSequence(A, length, intervalLen, MIN, MAX);
-    // PrintArr(A, length);
-    StepSequence(A, length, intervalLen, MIN, MAX);
-    PrintArr(A, length);
-} 
+//print double array to console
+void PrintArrConsoleDouble(double Array[], int len){
+    for (int i = 0; i < len; i++){
+        char end[3];
+        if (i != len - 1){
+            // end = ", ";
+            strcpy(end, ", ");
+        } else{
+            strcpy(end, "]\n");
+        }
+        printf("%.3f%s", Array[i], end);
+    }
+}
+
+//print double array to file
+void PrintArrFileDouble(double Array[], int len, FILE *file){
+    for (int i = 0; i < len; i++){
+        fprintf(file, "%.3f\n", Array[i]);
+    }
+    fprintf(file, "\n");
+}
+
+
+//generates an ascending double sequence
+void AscendingSequenceDouble(double Array[], int len){
+    double k = ((double)(MAX_DOUBLE - MIN_DOUBLE) - 0.125) / (double)(len), b = MIN_DOUBLE + 0.875;
+    for (int i = 0; i < len; i++){
+        Array[i] = k * i + b;
+    }
+}
+
+//generates a descending double sequence
+void DescendingSequenceDouble(double Array[], int len){
+    double k = ((MAX_DOUBLE - MIN_DOUBLE - 0.125) * -1.0) / (len * 1.0), b = MAX_DOUBLE - 0.125;
+    for (int i = 0; i < len; i++){
+        Array[i] = k * i + b;
+    }
+}
+
+//generates a random double sequence
+void RandomSequenceDouble(double Array[], int len){
+    srand(time(NULL));
+    for (int i = 0; i < len; i++){
+        Array[i] =  (double)(rand())/RAND_MAX * (MAX_DOUBLE - MIN_DOUBLE) + MIN_DOUBLE;
+    }
+}
+
+//generates a saw double sequence
+void SawSequenceDouble(double Array[], int len, int intervalLen, double min, double max){
+    double k = (max - min) / (double)(intervalLen);
+    for (int i = 0; i < len; i++){
+        Array[i] = (double)(k * (i % intervalLen)) + min;
+    }
+}
+
+//generates a sinusoidal double sequence
+void SinSequenceDouble(double Array[], int len, int intervalLen, double min, double max){
+    double x = 2 * M_PI / (intervalLen * 1.0);
+    double k = (max - min) / (double)(2);
+    for (int i = 0; i < len; i++){
+        Array[i] = k * sin(i * x) + min + k;
+    }
+}
+
+
+//generates a step double sequence
+void StepSequenceDouble(double Array[], int len, int intervalLen, double min, double max){
+    int intMin, intMax, intCount; 
+    double stepHigh;
+    if (len % intervalLen == 0){
+        intCount = len / intervalLen;
+    } else{
+        intCount = len / intervalLen + 1;
+    }
+    
+    stepHigh = (max - min) / (double)(intCount);
+    intMin = min;
+    intMax = min + stepHigh;
+
+    srand(time(NULL));
+    for (int i = 0; i < len; i++){
+        if (i > 0 && i < len - 1 && i % intervalLen == 0 && (i + 1) / intervalLen > 0){
+            intMin = intMax;
+            intMax += stepHigh;
+        }
+        Array[i] =  (double)(rand())/RAND_MAX * (intMax - intMin) + intMin;
+    }
+}
